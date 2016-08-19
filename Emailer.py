de3348b226c15f697dbf0b3ed92aaec540c299d6
Tom <thomasjackdalby@gmail.com>
2016-08-19 19:17:50 +0100

    work on client_template.py

--- Emailer.py
+++ Emailer.py
@@ -9,34 +9,37 @@ DEBUG = True
 
 class Emailer(object):
 
-	def send_email(self, to, subject, body):
-		if (self.isSetup == False):
-			print 'Need to setup first'
-			return
-	
-		#construct message
-		msg=MIMEText(body)
-		msg['Subject']=subject
-		msg['From']=self.username[0:-8]
-		msg['To']=to
-		
-		#connect to gmail
-		s=smtplib.SMTP('smtp.gmail.com')
-		s.starttls()
-		s.login(self.username, self.password)
-			
-		#send message
-		s.sendmail(self.username,to,msg.as_string())
-		s.quit()
-		print 'Sent %s @ %s' % (subject + ':' + body, datetime.datetime.now())
-	
-	def send_command(self, command, details):
-		self.send_email(server, command, details)
-	
-	def read_email(self):
-		print 'something'
-	
-	def setup(self, new_username, new_password):
-		self.username = new_username
-		self.password = new_password
-		self.isSetup = True
+    def send_email(self, to, subject, body):
+        if (self.isSetup == False):
+            print 'Need to setup first'
+            return
+    
+        #construct message
+        msg=MIMEText(body)
+        msg['Subject']=subject
+        msg['From']=self.username[0:-8]
+        msg['To']=to
+        
+        #connect to gmail
+        s=smtplib.SMTP('smtp.gmail.com')
+        s.starttls()
+        s.login(self.username, self.password)
+            
+        #send message
+        s.sendmail(self.username,to,msg.as_string())
+        s.quit()
+        print 'Emailed: %s' % (to);
+        print 'Date     %s' % (datetime.datetime.now());
+        print 'Subject: %s' % (subject);
+        print 'Body:    %s' % (body);
+    
+    def send_command(self, command, details):
+        self.send_email(server, command, details)
+    
+    def read_email(self):
+        print 'something'
+    
+    def setup(self, new_username, new_password):
+        self.username = new_username
+        self.password = new_password
+        self.isSetup = True
--- client_template.py
+++ client_template.py
@@ -1,29 +1,45 @@
-from Emailer import *
+import sys
+import Emailer
 import random
+from yahoo_finance import Share
 
 class Client(object):
 
-	def setup(self):
-		#Load password from local file
-		f = open('data.xtxt', 'r');
-		lines = f.read().split('\n');
-		for line in lines:
-			for char in '\r\n':
-				line.translate(None, char)
-	
-		self.emailer = Emailer()
-		self.emailer.setup(lines[0], lines[1])
-		
-	def run(self):
-		
-		stocks = ['HOG', 'RVT']
+    def setup(self):
+        #Load password from local file
+        f = open('data.xtxt', 'r');
+        lines = f.read().split('\n');
+        for line in lines:
+            for char in '\r\n':
+                line.translate(None, char)
 
-		self.emailer.send_command('reset', 'BOOM')
-		i = 0
-		while(i<0):
-			amount = random.randint(-5, 5);
-			self.emailer.send_command('buy', 'TNE.AX %s' % amount)
-			i = i+1
+        self.emailer = Emailer.Emailer()
+        self.emailer.setup(lines[0], lines[1])
+
+    def run(self):
+
+        #print self.check_price('BAB.L');
+
+
+        self.buy('BAB.L', 200)
+        self.buy('NIOBF', 200)
+        self.buy('RVT', 200)
+        self.buy('HOG', 'max')
+
+    def buy(self, ticker, amount):
+        self.emailer.send_command('buy', '%s %s' % (ticker, amount));
+
+    def sell(self, ticker, amount):
+        self.emailer.send_command('sell', '%s %s' % (ticker, amount));
+
+    def check_price(self, ticker):
+        try:
+            company = Share(ticker)
+            return float(company.get_price())
+        except:
+            print 'Failed to check price'
+            print sys.exc_info()[0];
+            return 0
 
 c = Client();
 c.setup();
